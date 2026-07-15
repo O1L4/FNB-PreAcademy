a823f3dc589222448aa84915ac0baea001a9bdcf
O1L4 <140461115+O1L4@users.noreply.github.com>
2026-07-15 12:43:04 +0200

    Add files via upload
    Assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,8 @@
+#A digital ticket counter
+
+first_name = input("Enter your first name:")
+name_of_artist = input("Enter artist:")
+
+#Confimation message using f-string
+
+print(f"Hey {first_name}! Your tickets to see {name_of_artist} are booked successfully!") 
